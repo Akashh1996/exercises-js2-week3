489132618c23ceaf76fd95e9afe69d1e6ce475fb
Akash <akashh.sapkota@gmail.com>
2020-05-06 19:11:03 +0200

    done

--- InClass/A-timeout/exercise.js
+++ InClass/A-timeout/exercise.js
@@ -1,4 +1,4 @@
-/*
+/* 
 ================
 EXERCISE 1
 
@@ -7,5 +7,20 @@ Using setTimeout, change the background colour of the page after 5 seconds (5000
 
 Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
-================
-*/
+================ */
+
+/* var body = document.querySelector("body")
+ setTimeout(function back(){
+
+    body.style.background = "red"
+},5000) 
+
+ */
+setInterval(function back(){
+    var colors = ["red","blue","green","yellow","silver"]
+    let x = (Math.ceil(Math.random()*5))
+    console.log(x);
+    
+    document.body.style.background = colors[x]
+},500)
+
